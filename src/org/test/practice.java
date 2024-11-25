d58517b71328fdf90c446c379dbf730941891cc8
im-bhuvi <bhuvanesh.bhuvi1606@gmail.com>
2024-11-25 13:52:41 +0530

    Update practice.java

--- src/org/test/practice.java
+++ src/org/test/practice.java
@@ -3,8 +3,9 @@ package org.test;
 public class practice {
 	public static void main(String[] args) {
 		int a=10
+		int b=11
 		System.out.println(a);
-		System.out.println("done");
+		System.out.println(b);
 	}
 
 }
